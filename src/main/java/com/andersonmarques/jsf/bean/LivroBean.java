58d0602bb4760380517b4ede90d0398d10ad6046
AndersonMarquess <andersongabriel96.ag@gmail.com>
2019-02-28 22:14:40 -0300

    Criado ligação entre autor e livro no cadastro de livro

--- src/main/java/com/andersonmarques/jsf/bean/LivroBean.java
+++ src/main/java/com/andersonmarques/jsf/bean/LivroBean.java
@@ -1,16 +1,43 @@
 package com.andersonmarques.jsf.bean;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
 import com.andersonmarques.jsf.dao.DAO;
+import com.andersonmarques.jsf.model.Autor;
 import com.andersonmarques.jsf.model.Livro;
 
 //Informa que a classe é gerenciada pelo JSF
 @SuppressWarnings("deprecation")
 @ManagedBean
+@SessionScoped//Preserva o livroBean enquanto a tela .xhtml existir
 public class LivroBean {
 	
 	private Livro livro = new Livro();
+	private Integer autorId;
+
+	public Integer getAutorId() {
+		return autorId;
+	}
+	
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
+	}
+	
+	public List<Autor> getAutores() {
+		return new DAO<Autor>(Autor.class).listarTodos();
+	}
+	
+	public void gravarAutor() {
+		Autor autor = new DAO<Autor>(Autor.class).buscarPorId(autorId);
+		livro.adicionarAutor(autor);
+	}
+	
+	public List<Autor> getAutoresDoLivro() {
+		return livro.getAutores();
+	}
 	
 	//Retorna o livro para a View
 	public Livro getLivro() {
@@ -23,5 +50,6 @@ public class LivroBean {
         }
 		
 		new DAO<Livro>(Livro.class).gravar(livro);
+		livro = new Livro();
 	}
 }
