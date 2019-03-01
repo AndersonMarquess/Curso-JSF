5a2ef0e91b711a2745118611df93939ac7cca522
AndersonMarquess <andersongabriel96.ag@gmail.com>
2019-03-01 20:37:12 -0300

    Criado tabela com lista de livros

--- src/main/java/com/andersonmarques/jsf/bean/LivroBean.java
+++ src/main/java/com/andersonmarques/jsf/bean/LivroBean.java
@@ -3,7 +3,7 @@ package com.andersonmarques.jsf.bean;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 
 import com.andersonmarques.jsf.dao.DAO;
 import com.andersonmarques.jsf.model.Autor;
@@ -11,44 +11,49 @@ import com.andersonmarques.jsf.model.Livro;
 
 //Informa que a classe é gerenciada pelo JSF
 @SuppressWarnings("deprecation")
+@ViewScoped // Preserva o livroBean enquanto a tela .xhtml existir
 @ManagedBean
-@SessionScoped//Preserva o livroBean enquanto a tela .xhtml existir
 public class LivroBean {
-	
+
 	private Livro livro = new Livro();
 	private Integer autorId;
 
 	public Integer getAutorId() {
 		return autorId;
 	}
-	
+
 	public void setAutorId(Integer autorId) {
 		this.autorId = autorId;
 	}
-	
+
 	public List<Autor> getAutores() {
 		return new DAO<Autor>(Autor.class).listarTodos();
 	}
-	
+
 	public void gravarAutor() {
 		Autor autor = new DAO<Autor>(Autor.class).buscarPorId(autorId);
 		livro.adicionarAutor(autor);
 	}
-	
+
 	public List<Autor> getAutoresDoLivro() {
 		return livro.getAutores();
 	}
-	
-	//Retorna o livro para a View
+
+	public List<Livro> getLivros() {
+		List<Livro> livros = new DAO<Livro>(Livro.class).listarTodos();
+		return livros;
+	}
+
+	// Retorna o livro para a View
 	public Livro getLivro() {
 		return livro;
 	}
 
 	public void gravar() {
-		if(livro.getAutores().isEmpty()) {
-            throw new RuntimeException("O livro deve ter pelo menos um Autor");
-        }
-		
+		if (livro.getAutores().isEmpty()) {
+			throw new RuntimeException("O livro deve ter pelo menos um Autor");
+		}
+
 		new DAO<Livro>(Livro.class).gravar(livro);
 		livro = new Livro();
 	}
