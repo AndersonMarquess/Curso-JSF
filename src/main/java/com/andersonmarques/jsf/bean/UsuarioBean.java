a06897d7707569dd0985dc25d055f96d08fdb1a7
AndersonMarquess <andersongabriel96.ag@gmail.com>
2019-03-09 13:38:06 -0300

    Criado método para autenticar usuário

--- src/main/java/com/andersonmarques/jsf/bean/UsuarioBean.java
+++ src/main/java/com/andersonmarques/jsf/bean/UsuarioBean.java
@@ -1,9 +1,12 @@
 package com.andersonmarques.jsf.bean;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 import com.andersonmarques.jsf.dao.DAO;
 import com.andersonmarques.jsf.model.Usuario;
+import com.andersonmarques.jsf.util.RedirecionarPagina;
 
 @SuppressWarnings("deprecation")
 @ManagedBean
@@ -12,9 +15,17 @@ public class UsuarioBean {
 	private DAO<Usuario> usuarioDAO = new DAO<>(Usuario.class); 
 	private Usuario usuario = new Usuario();
 	
-	public void login() {
-		System.out.println("Verificando validade do usuário");
-		//usuarioDAO.isUsuarioCadastrado(usuario);
+	public Usuario getUsuario() {
+		return usuario;
 	}
 	
+	public String login() {
+		
+		if(usuarioDAO.isUsuarioCadastrado(usuario)) {
+			return RedirecionarPagina.destino("livro");
+		}
+		
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Usuário inválido."));
+		return null;
+	}
 }
